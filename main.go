b2538b75dd9c2f95e278239b48eae05e7a431fa2
SRAUFactory <srau.factory@gmail.com>
2018-05-18 15:59:47 +0900

    feat: [WIP]add `requestHttp`

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import "flag"
 import "fmt"
+import "io"
 
 var (
 	url      = flag.String("u", "", "Invalid value are http(s)~")
@@ -10,6 +11,23 @@ var (
 	params   = flag.String("p", "", "If 't' is 'params', this parameter is required. Set request parameters")
 	path     = flag.String("f", "", "If 't' is 'path', this parameter is required. Set file path to export for request body")
 )
+
+func requestHttp(url string, method string, body string) ([]byte, error) {
+	req, err := http.NewRequest("GET", method, bytes.NewBuffer([]byte(body)))
+	if err != nil {
+		return nil, err
+	}
+
+	client := new(http.Client)
+	resp, err := client.Do(req)
+	if err != nil {
+		return nil, err
+	}
+
+	defer resp.Body.Close()
+
+	return ioutil.ReadAll(resp.Body), nil
+}
 
 func main() {
 	flag.Parse()
